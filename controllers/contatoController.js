1c995046833a7a88bd4131a412ab34977be71673
henr1qu3 <henrique.nogueira@pge.ro.gov.br>
2020-11-27 11:29:43 -0400

    Validação dos campos de Fale Conosco

--- controllers/contatoController.js
+++ controllers/contatoController.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const ejs = require('ejs');
+const flash = require('express-flash');
+
+const { text } = require('body-parser');
+const { default: validator } = require('validator');
 
 router.get('/contato', (req, res) => {
     res.render('contato')
-})
+});
 
+router.post('/contato', (req, res) => {
+    var erros = [];
+
+    var {nomeContato, emailContato, msgContato} = req.body;
+
+    if(!nomeContato || typeof nomeContato == undefined || nomeContato == null){
+        erros.push({text: 'Nome inválido'});
+    }
+    if (typeof emailContato == undefined || !emailContato || emailContato == null) {
+        erros.push({text: 'E-mail inválido'});
+    }
+    if(validator.isEmpty(msgContato)){
+        erros.push({text: 'O campo não pode ser vazio'});
+    }
+
+    if(erros.length > 0){
+        res.render('contato', {erros, nomeContato, emailContato, msgContato});
+    }else{
+        req.flash('msg_sucesso', 'Mensagem enviada!');
+        //no caso, redirecionar para contato
+        res.redirect('/contato')
+    }
+})
 
 module.exports = router;
