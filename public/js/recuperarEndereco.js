281ed1d4ed1e7835e478254ccdc8adbba50c9ef2
henr1qu3 <henrique.nogueira@pge.ro.gov.br>
2020-12-03 12:01:29 -0400

    função incompleta

--- public/js/recuperarEndereco.js
+++ public/js/recuperarEndereco.js
@@ -1,33 +1,42 @@
+
 export const recuperarEndereco = (input) => {
     const cepNumeros = input.value.replace(/\D/g, "");
 
-    if(input.validity.valid){
-       /* const url = `http://cep.la/${cepNumeros}/json/`;
-        const options = {
-            method: 'GET',
-            mode: "cors",
-            headers: {
-                'content-type': 'application/json;charset=utf-8'
+    https://developer.chrome.com/apps/xhr
+
+       /*
+        const cep = document.querySelector("#cep")
+
+        const showData = (result)=>{
+            for(const campo in result){
+                if(document.querySelector("#"+campo)){
+                    document.querySelector("#"+campo).value = result[campo]
+                }
             }
-        };
-        fetch(url, options)
-        .then(response => response.json())
-        .then(data => {
-            if(data.erro){
-                input.setCustomValidity("Este não é um Cep válido");
-                return
+        }
+
+        cep.addEventListener("blur",(e)=>{
+            let search = cep.value.replace("-","")
+            const options = {
+                method: 'GET',
+                mode: 'cors',
+                cache: 'default'
             }
-            preecherCampos(data);
-            input.setCustomValidity("");
-                return
-        });
+            
+
+            fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+            .then(response =>{ response.json()
+                .then( data => showData(data))
+            })
+            .catch(e => console.log('Deu Erro: '+ e,message))
+        })
         */
-       const xhr = new XMLHttpRequest();
+       var xhr = new XMLHttpRequest();
         xhr.open ("GET", `http://cep.la/${cepNumeros}`, true);
         xhr.setRequestHeader ("Accept", "application/json");
         xhr.onreadystatechange = function(){
             if((xhr.readyState == 0 || xhr.readyState == 4) && xhr.status == 200){
-                preecherCampos(xhr);
+                preecherCampos(input);
                 input.setCustomValidity("");
                 return
             }else{
@@ -37,18 +46,18 @@ export const recuperarEndereco = (input) => {
             }    
         };
         xhr.send(null);
-    }
+    
 };
     
 
-const preecherCampos = (xhr) => {
+const preecherCampos = (input) => {
     const campoEndereco1 = document.getElementById("endereco1");
     const campoCidade = document.getElementById("cidade");
     const campoEstado = document.getElementById("estado");
 
-    campoEndereco1.value = xhr.logradouro;
-    campoCidade.value = xhr.cidade;
-    campoEstado.value = xhr.uf;
+    campoEndereco1.value = xhr.responseText("logradouro");
+    campoCidade.value = xhr.responseText("cidade");
+    campoEstado.value = xhr.responseText("uf")
 
 
 }
